9f87c932ba5460243bbed631a9e98b3a88c7ceba
= <=>
2021-01-23 20:11:42 -0300

    updating file

--- vitorworkstation.sh
+++ vitorworkstation.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 ## Removendo travas do apt ##
-sudo rm/var/lib/dpkg/lock-frontend ; sudo rm /var/cache/apt/archives/lock ;
+sudo rm /var/lib/dpkg/lock-frontend ; sudo rm /var/cache/apt/archives/lock ;
 
 ## Atualizando o repositório ##
 sudo apt update &&
+
+##Pacotes do repositório deb do Ubuntu
+sudo apt install python3 docker docker-compose git build-essential libssl-dev flatpak gnome-software-plugin-flatpak -y &&
 
 ## Instalando snap ##
 sudo apt install snapd
@@ -16,6 +19,7 @@ sudo snap install code --classic &&
 sudo snap install --edge node --classic &&
 sudo snap install spotify &&
 sudo snap install wps-office-multilang &&
+sudo snap install insomnia
 
 ## Adicionando repositório Flathub ##
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.comflatpakrepo &&
@@ -26,9 +30,6 @@ sudo flatpak install flathub com.sublimetext.three -y &&
 
 ## Softwares de download externo ##
 cd ~/Downloads/ && wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i *.deb && wget -c https://uploads.treeunfe.me/downloads/instalar-freenfe.exe &&
-
-## GIMP e PhotoGIMP ##
-flatpak install flathub org.gimp.GIMP -y && wget -c https://doc-0s-1g-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/0v83rmt4mij9897co9ufvor2r1jcj1am/1567965600000/07452089978596344616/*/12i-ihCDSZelx30-oNHJaKAzUei1etsbS?e=download && unzip 12i-ihCDSZelx30-oNHJaKAzUei1etsbS?e=download && cd "PHOTOGIMP V2018 - DIOLINUX" && cd "PATCH" && mkdir -p /home/$USER/.var/app/org.gimp.GIMP/config/GIMP/2.10/ && cp -R * /home/$USER/.var/app/org.gimp.GIMP/config/GIMP/2.10/ &&
 
 ## Atualização do sistema ##
 sudo apt update && sudo apt dist-upgrade -y && sudo apt autoclean -y && sudo apt autoremove -y &&
