039613bf3dca50c015e5997c7201e0909287a880
Vitor Negromonte <61950660+vitornegromonte@users.noreply.github.com>
2021-02-05 23:57:41 -0300

    Update

--- vitorworkstation.sh
+++ vitorworkstation.sh
@@ -7,19 +7,16 @@ sudo rm /var/lib/dpkg/lock-frontend ; sudo rm /var/cache/apt/archives/lock ;
 sudo apt update &&
 
 ##Pacotes do repositório deb do Ubuntu
-sudo apt install python3 docker docker-compose git build-essential libssl-dev flatpak gnome-software-plugin-flatpak -y &&
+sudo apt install python3 git build-essential libssl-dev flatpak gnome-software-plugin-flatpak -y &&
 
 ## Instalando snap ##
 sudo apt install snapd
 
 ## Instalando pacotes Snap ##
-sudo snap install slack --classic &&
 sudo snap install discord &&
 sudo snap install code --classic &&
 sudo snap install --edge node --classic &&
 sudo snap install spotify &&
-sudo snap install wps-office-multilang &&
-sudo snap install insomnia &&
 sudo snap install postman
 
 ## Adicionando repositório Flathub ##
@@ -27,10 +24,23 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.comf
 
 ## Instalando Apps do Flathub ##
 sudo flatpak install flathub com.obsproject.Studio -y &&
-sudo flatpak install flathub com.sublimetext.three -y &&
 
-## Softwares de download externo ##
-cd ~/Downloads/ && wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i *.deb && wget -c https://uploads.treeunfe.me/downloads/instalar-freenfe.exe &&
+## Instalando Sublime Text 3 via pacote oficial
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add - && 
+sudo apt-get install apt-transport-https && 
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list && 
+sudo apt-get update &&
+sudo apt-get install sublime-text
+
+## Instalando pacote Ubuntu Make
+sudo apt-get install ubuntu-make && sudo apt-get update
+
+## Instalando linguagem Dart
+sudo sh -c 'wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -' &&
+sudo sh -c 'wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list' &&
+sudo apt-get update
+sudo apt-get install dart
+
 
 ## Atualização do sistema ##
 sudo apt update && sudo apt dist-upgrade -y && sudo apt autoclean -y && sudo apt autoremove -y &&
