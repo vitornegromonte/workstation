fb8a3416f39a52d91485490c40fe0e3ce2554db0
vitornegromonte <vitorncabral@protonmail.com>
2021-02-07 13:30:31 -0300

    Update

--- vitorworkstation.sh
+++ vitorworkstation.sh
@@ -38,6 +38,13 @@ sudo apt-get update
 sudo apt-get install dart
 echo "Dart-lang instalada com sucesso"
 
+## Instalando Brave web-browser pelo pacote oficial##
+sudo apt install apt-transport-https curl gnupg &&
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add - &&
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list &&
+sudo apt update &&
+sudo apt install brave-browser &&
+
 ## Atualização do sistema ##
 sudo apt update && sudo apt dist-upgrade -y && sudo apt autoclean -y && sudo apt autoremove -y &&
 
